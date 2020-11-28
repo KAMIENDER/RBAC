fb3d0c49c596cbe2fb09a1e0f2d4c1e5bf5341f5
hejiadong <hejiadong@bytedance.com>
2020-11-28 14:42:05 +0800

    fix: 数据库修复

--- manage.py
+++ manage.py
@@ -4,8 +4,13 @@ from flask_script import Manager
 from infrastructure.config.database_config import db
 from infrastructure.config.flask import app
 
+from domains.item.models.item import Item
+from domains.item.models.item_ref import ItemRef
+from domains.permission.models.permission import Permission
+from domains.user.models.user import User
+
 manager = Manager(app)
-migrate = Migrate(app, db)
+migrate = Migrate(app, db, render_as_batch=True, compare_type=True, compare_server_default=True)
 
 manager.add_command('db', MigrateCommand)
 
